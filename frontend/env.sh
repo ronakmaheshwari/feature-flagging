2bfd80bb844645c4be1ffba6f2424dd4ed036fb9
ronak maheshwari <ronakmaheshwari077@gmail.com>
2026-09-03 09:19:02 +0530

    feat: Completed docker setup for sandboxing

--- frontend/env.sh
+++ frontend/env.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-echo "API_URL is: $VITE_API_URL"
+echo "API_URL is: $import.meta.VITE_API_URL"
 
 cat <<EOF > /usr/share/nginx/html/env-config.js
 window.env_ = {
-  VITE_API_URL: "${VITE_API_URL}",
+  VITE_API_URL: "${import.meta.VITE_API_URL}",
 };
 
 console.log("Loaded env config:", window.env_);
